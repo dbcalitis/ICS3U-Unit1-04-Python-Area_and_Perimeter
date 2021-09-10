0fd160b8333375652bd5fc23fc41962f4d7d2227
Ubuntu <ubuntu@ip-172-31-81-123.ec2.internal>
2021-09-10 22:30:16 +0000

    added area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Daria Bernice Calitis
+#
+# Created by: Daria Bernice Calitis
+# Created on: Sept 2021
+# This program calculates the area and perimeter of a rectangle
+#    with the dimensions of 8 cm and 7 cm
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("The rectangle's dimensions are:")
+    print("8cm x 7cm\n")
+    print("The area is {}cm².".format(8 * 7))
+    print("The perimeter is {}cm.".format(2 * (8 + 7)))
+
+
+if __name__ == "__main__":
+    main()
